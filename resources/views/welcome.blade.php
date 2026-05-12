141651fa3b0e7f3923248eed8709e920e7f3df4c
https://github.com/KhayPhat <dinhlephatdanthuong@gmail.com>
2026-05-12 16:07:04 +0700

    Init commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Thông tin cá nhân</title>
+
+    <style>
+        body{
+            font-family: Arial;
+            background-color: #f2f2f2;
+            text-align: center;
+            padding-top: 50px;
+        }
+
+        .card{
+            width: 500px;
+            margin: auto;
+            background: white;
+            padding: 30px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px gray;
+        }
+
+        h1{
+            color: blue;
+        }
+
+        p{
+            font-size: 18px;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="card">
+        <h1>THÔNG TIN CÁ NHÂN</h1>
+
+        <p><b>Họ tên:</b> Đinh Lê Phát</p>
+        <p><b>Lớp:</b> CCQ2211B</p>
+        <p><b>Email:</b> dinhlephat@gmail.com</p>
+        <p><b>Sở thích:</b> Lập trình, chơi game, nghe nhạc</p>
+    </div>
+
+</body>
+</html>
